04ce01a5387eeb84b7afc7bb52aad97e807fb2c5
Jesús Valdez <jsvaldezv@yahoo.com>
2022-07-12 14:22:40 -0500

    Contador working

--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import "./ItemListContainer.css"
 
+import ItemCount from "../ItemCount/ItemCount.js"
+
 function ItemListContainer({title}) 
 {
 	return (
 		<div className="itemListContainer">
+
 			<h1>{title}</h1>
+
+			<ItemCount stock={5} initial={1}/>
+
 		</div>
 	)
 }
