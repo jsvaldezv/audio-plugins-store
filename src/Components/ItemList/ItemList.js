38286f2b9235a13a37ae84d562ce043ef05b941e
Jesús Valdez <jsvaldezv@yahoo.com>
2022-07-20 12:24:11 -0500

    Corrección map and async

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -1,31 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import "./ItemList.css"
 
 import Item from "./Item/Item.js"
-import { products } from './Productos.js';
 
-const getProducts = () => {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			resolve(products);
-		}, 2000)
-	})
-}
-
-function ItemList() 
+function ItemList({products, isLoading}) 
 {
-	const [productsArray, setProductsArray] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
-
-	useEffect(() => {
-		getProducts()
-		.then(respuesta => {
-			setProductsArray(respuesta)
-			setIsLoading(false)
-		})
-		.catch(err => console.log(err))
-	}, []);
-
 	return (
 
 		<div className="itemList">
@@ -34,7 +13,7 @@ function ItemList()
 				<h2>Loading...</h2> 
 				: 
 				<ul>
-					{productsArray.map(product => 
+					{products.map(product => 
 						<li key={product.id}> 
 							<Item 	title={product.name} 
 									price={product.price} 
--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./ItemListContainer.css"
 
 import ItemCount from "../ItemCount/ItemCount.js"
 import ItemList from "../ItemList/ItemList.js"
 
+import { products } from '../ItemList/Productos.js';
+
+const getProducts = () => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(products);
+		}, 2000)
+	})
+}
+
 function ItemListContainer({title}) 
 {
+	const [productsArray, setProductsArray] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		getProducts()
+		.then(respuesta => {
+			setProductsArray(respuesta)
+			setLoading(false)
+		})
+		.catch(err => console.log(err))
+	}, []);
+
 	return (
 
 		<div className="itemListContainer">
@@ -14,7 +36,7 @@ function ItemListContainer({title})
 
 			<ItemCount stock={5} initial={1}/>
 
-			<ItemList />
+			<ItemList products={productsArray} isLoading={loading}/>
 
 		</div>
 
