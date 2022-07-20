7cdd9c7019817f55316aec2861888668c47e6335
Jesús Valdez <jsvaldezv@yahoo.com>
2022-07-20 12:36:28 -0500

    Correción async

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import NavBar from "./Components/NavBar/NavBar.js"
 import ItemListContainer from "./Components/ItemListContainer/ItemListContainer.js"
 import ItemDetailContainer from "./Components/ItemDetailContainer/ItemDetailContainer.js"
 
-import { products } from './Components/Productos/Productos.js';
-
-const getProducts = () => {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			resolve(products);
-		}, 2000)
-	})
-}
-
 function App() 
 {
-	const [productsArray, setProductsArray] = useState([]);
-
-	useEffect(() => {
-		getProducts()
-		.then(respuesta => {
-			setProductsArray(respuesta)
-		})
-		.catch(err => console.log(err))
-	}, []);
-
 	return (
 
 		<div className="App">
@@ -34,7 +14,7 @@ function App()
 
 			{/* <ItemListContainer title={"Contador"}/> */}
 
-			<ItemDetailContainer inProducts={productsArray}/>
+			<ItemDetailContainer />
 
 		</div>
 		
--- src/Components/ItemDetailContainer/ItemDetailContainer.js
+++ src/Components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./ItemDetailContainer.css"
 
 import ItemDetail from "./ItemDetail/ItemDetail.js"
 
-function ItemDetailContainer({inProducts}) 
+import { products } from '../Productos/Productos.js';
+
+const getProducts = () => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(products);
+		}, 2000)
+	})
+}
+
+function ItemDetailContainer() 
 {
+	const [productsArray, setProductsArray] = useState([]);
+
+	useEffect(() => {
+		getProducts()
+		.then(respuesta => {
+			setProductsArray(respuesta)
+		})
+		.catch(err => console.log(err))
+	}, []);
+
 	return (
 
 		<div className="itemDetailContainer">
-			{inProducts.map(product => 
+			{productsArray.map(product => 
 					<ItemDetail name={product.name} price={product.price} description={product.categoria}/> 
 			)}
 		</div>
