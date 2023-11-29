fd399f37e8e0d25726a1e8253aa4f4b2966f5ec1
Jesús Valdez <jsvaldezv@yahoo.com>
2023-11-29 12:00:34 -0600

    Update config.js

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app"
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDygXxewwZgV6RD1Aj0pi5INxwC48Rt_tw",
-	authDomain: "plugins-store.firebaseapp.com",
-	projectId: "plugins-store",
-	storageBucket: "plugins-store.appspot.com",
-	messagingSenderId: "947884578358",
-	appId: "1:947884578358:web:fabe6c7d7a0247d3852a97",
-	measurementId: "G-PZDKSH6QYM"
+	apiKey: "",
+	authDomain: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: "",
+	measurementId: ""
   };
 
 const app = initializeApp(firebaseConfig);
